127e33997a93cdbe35b5a1511d08db2c6a7bab2e
yotamcohen1 <yotamco@access2.wexac.weizmann.ac.il>
2021-11-06 20:02:00 +0200

    Described running a file

--- run_sample.sh
+++ run_sample.sh
@@ -1,1 +1,1 @@
-﻿diamond blastx -d ~/Dietery_fiber/ec_full -q [INPUt_FILE] -k 1 -p 20 -o [OUTPUT_FILE] -f 6; ~/EC/ecn_map.sh [OUTPUT_FILE]
+diamond blastx -d [DATABASE] -q [INPUt_FILE] -k 1 -p 20 -o [OUTPUT_FILE] -f 6; ecn_map.sh [OUTPUT_FILE]
