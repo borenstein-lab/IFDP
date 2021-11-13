8fc86267b53af8e7eef9133c4def48c6e18add12
yotamcohen1 <yotamco@access2.wexac.weizmann.ac.il>
2021-11-13 10:41:24 +0200

    Completed the simple pipeline

--- ecn_map.sh
+++ ecn_map.sh
@@ -2,5 +2,5 @@ if [ ! -f $1 ]; then
     echo "File" $1 " not found!";exit
 fi
 
-less $1 |awk '{print $2}'|awk -F '|' '{print $2}' | python ~/EC/map_diamond_ec.py $1_ec.tsv
+less $1 |awk '{print $2}'|awk -F '|' '{print $2}' | python count_ec.py $1_counts.tsv
 
--- run_sample.sh
+++ run_sample.sh
@@ -1,1 +1,9 @@
-diamond blastx -d [DATABASE] -q [INPUt_FILE] -k 1 -p 20 -o [OUTPUT_FILE] -f 6; ecn_map.sh [OUTPUT_FILE]
+if [ ! -f $1 ]; then
+    echo "File" $1 " not found!";exit
+fi
+
+if [ ! -f $2 ]; then
+    echo "File" $2 " not found!";exit
+fi
+
+diamond blastx -d $1 -q $2 -k 1 -p 20 -o $3 -f 6 ; ./ecn_map.sh $3
