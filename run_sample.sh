e4b4826757558fb6d427ca7c06e8c5fa869b2b96
yotamcohen1 <33667593+yotamcohen1@users.noreply.github.com>
2022-10-11 16:27:33 +0300

    Update run_sample.sh

--- run_sample.sh
+++ run_sample.sh
@@ -1,4 +1,6 @@
-while getopts i:o:d:p: flag
+threads=20
+
+while getopts :i:o:d:p: flag
 do
     case "${flag}" in
         i) input=${OPTARG};;
@@ -8,6 +10,6 @@ do
     esac
 done
 
-threads=20
+
 
 diamond blastx -d $database -q $input -k 1 -p $threads -o $output -f 6 ; sh ecn_map.sh $output
